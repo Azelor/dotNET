210b505b8121abd0b82228384719a3f437cfbc63
Azelor <kaarel.soot@gmail.com>
2016-04-16 21:33:51 +0300

    Add while iteration

--- ConsoleApplication2/ConsoleApplication2/Program.cs
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -9,7 +9,30 @@ namespace ConsoleApplication2
     class Program
     {
         static void Main(string[] args)
+        { // Asks for a user input
+            Console.WriteLine("Enter something to be reversed.");
+            string userInput = Console.ReadLine();
+
+            // Calls the method displayResult which calls the method reverseString
+            displayResult(reverseString(userInput));
+        }
+
+        static string reverseString(string message)
         {
+            // Converts a string to an array of characters
+            char[] charArray = message.ToCharArray();
+            // Reverses the array
+            Array.Reverse(charArray);
+            // Converts the reversed array into a string
+            return String.Concat(charArray);
+
+            
+        }
+
+        static void displayResult(string message)
+        {
+            Console.WriteLine(message);
+            Console.ReadLine();
         }
     }
 }
