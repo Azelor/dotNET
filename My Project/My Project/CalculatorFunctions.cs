fe17d380d9b2ef1698aae5ce787e93768fbb6ba8
Kaarel Sööt <kaarel.soot@gmail.com>
2016-07-12 14:23:55 +0300

    Add My Project solution

---
+++ My Project/My Project/CalculatorFunctions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace My_Project
+{
+    public class CalculatorFunctions
+    {
+        public double Add(double FirstNumber, double SecondNumber)
+        {
+            return FirstNumber + SecondNumber;
+        }
+        public double Subtract(double FirstNumber, double SecondNumber)
+        {
+            return FirstNumber - SecondNumber;
+        }
+        public double Multiply(double FirstNumber, double SecondNumber)
+        {
+            return FirstNumber * SecondNumber;
+        }
+        public double Divide(double FirstNumber, double SecondNumber)
+        {
+            return FirstNumber / SecondNumber;
+        }
+    }
+}
---
+++ My Project/My Project/MyClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text;
+
+namespace CustomClasses.TestClasses
+{
+    public class MyClass
+    {
+        public int IntProperty;
+        public void AddToProperty()
+        {
+            IntProperty = IntProperty + 1; 
+        }
+        public void TestMethod()
+        {
+            StringBuilder TestStringBuilder = new StringBuilder();
+
+        }
+    }
+}
