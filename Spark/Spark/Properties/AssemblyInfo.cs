2075a6e6b57c22a7bcdbfa6be5fb34e89f1777b9
Azelor <kaarel.soot@gmail.com>
2016-08-14 13:54:41 +0300

    Add Spark project

--- Spark/Spark/Properties/AssemblyInfo.cs
+++ Spark/Spark/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SimpleMethod")]
+[assembly: AssemblyTitle("Spark")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SimpleMethod")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("Spark")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3df1013f-d9c5-4177-a037-f090d2d85c98")]
+[assembly: Guid("4aa88531-b700-4afe-b352-b623b3d63fe0")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,8 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Spark/Spark/newtransaction.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Spark
+{
+    public partial class newtransaction : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Calculate VAT for the specified Amount
+        /// </summary>
+        /// <param name="Amount">Amount to calculate VAT for</param>
+        /// <returns></returns>
+        private decimal CalculateVAT(decimal Amount)
+        {
+            // VAT or Value Added Tax is a sales tax levied in Europe
+            decimal VATAmount = Amount * 0.2m;
+            VATAmount = Math.Round(VATAmount, 2);
+            return VATAmount;
+        }
+
+        protected void ButtonSubmit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (DropDownListCustomer.SelectedValue == "6"
+                    || DropDownListCustomer.SelectedValue == "9"
+                    || DropDownListCustomer.SelectedValue == "11")
+                {
+                    LabelError.Text = "That customer is currently out of use";
+                }
+                else
+                {
+                }
+            }
+            catch (Exception Ex)
+            {
+                LabelError.Text = Ex.Message;
+            }
+        }
+    }
+}
