6ff9c8c300c4bb48a94feea2bcd95a2e7e81819e
Azelor <kaarel.soot@gmail.com>
2016-09-06 10:49:13 +0300

    Add new error page

--- SmartMethodStore/SmartMethodStore/Global.asax.cs
+++ SmartMethodStore/SmartMethodStore/Global.asax.cs
@@ -28,7 +28,7 @@ namespace SmartMethodStore
         void Application_Error(object sender, EventArgs e)
         {
             // Code that runs when an unhandled error occurs
-
+            Response.Redirect("~/error.aspx");
         }
     }
 }
