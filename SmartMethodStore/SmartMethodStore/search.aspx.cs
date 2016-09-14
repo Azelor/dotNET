15327e55ab0c0840d790de83ee8a62dabbaae317
Azelor <kaarel.soot@gmail.com>
2016-09-14 14:47:15 +0300

    Add Search page

--- SmartMethodStore/SmartMethodStore/search.aspx.cs
+++ SmartMethodStore/SmartMethodStore/search.aspx.cs
@@ -13,5 +13,43 @@ namespace SmartMethodStore
         {
 
         }
+
+        protected void GridViewProduct_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "AddToCart")
+            {
+                int RowClicked = Convert.ToInt32(e.CommandArgument);
+                int ProductID = Convert.ToInt32(GridViewProduct.DataKeys[RowClicked].Value);
+                List<int> ProductsInCart = (List<int>)Session["Cart"];
+                if (ProductsInCart == null)
+                {
+                    ProductsInCart = new List<int>();
+                }
+                ProductsInCart.Add(ProductID);
+                Session["Cart"] = ProductsInCart;
+            }
+        }
+
+        protected void ButtonSearch_Click(object sender, EventArgs e)
+        {
+            using (StoreDataContext Data = new StoreDataContext())
+            {
+                string ProductName = TextBoxProductName.Text;
+                decimal? PriceBelow = null;
+                if(TextBoxPriceBelow.Text.Length > 0)
+                {
+                    PriceBelow = Convert.ToDecimal(TextBoxPriceBelow.Text);
+                }
+                var SearchResults = Data.Products.Where
+                    (Product => 
+                    (Product.ProductName.Contains(ProductName) 
+                    || ProductName.Length == 0) 
+                    && 
+                    (Product.ProductPrice <= PriceBelow 
+                    || PriceBelow == null));
+                GridViewProduct.DataSource = SearchResults;
+                GridViewProduct.DataBind();
+            }
+        }
     }
 }
